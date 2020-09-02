4299725a501596f56416dbb3f271007b95a37f22
relative <relative@riseup.net>
2020-09-02 19:09:07 -0400

    invites: invite wave POST /invites/wave

--- src/routes/InvitesRouter.ts
+++ src/routes/InvitesRouter.ts
@@ -3,6 +3,8 @@ import express from 'express'
 import { authMiddleware, userIsAdmin } from '../util/Middleware'
 import { Invite } from '../database/entities/Invite'
 import bodyParser from 'body-parser'
+import { User } from 'src/database/entities/User'
+import { randomBytes } from 'src/util/RandomUtil'
 
 const InvitesRouter = express.Router()
 
@@ -36,6 +38,26 @@ InvitesRouter.route('/').get(userIsAdmin(), async (req, res) => {
     total: count,
     page: page,
     pages: Math.ceil(count / limit) - 1,
+  })
+})
+
+InvitesRouter.route('/wave').post(userIsAdmin(), async (req, res) => {
+  let maxUid = parseInt((req.query.maxUid as string) || '10000')
+  let users = await User.find({
+    where: {
+      id: `< ${maxUid}`,
+    },
+  })
+  users.forEach(async (user) => {
+    let invite = new Invite()
+    invite.invite = randomBytes(20)
+    invite.createdAt = new Date()
+    invite.creator = user.id
+    await invite.save()
+  })
+  return res.json({
+    success: true,
+    message: `started invite wave for uid < ${maxUid}`,
   })
 })
 
