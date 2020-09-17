bc18bd9c2e639c13e735a1868f2437b8bc034f07
relative <relative@pxl.blue>
2020-09-17 13:13:38 -0400

    feat: respect range header when serving images allows seeking of video files

--- src/routes/ProxyRouter.ts
+++ src/routes/ProxyRouter.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { Image } from '../database/entities/Image'
 import { bucket } from '../util/StorageUtil'
+import fetch from 'node-fetch'
+import moment from 'moment'
 import rb from 'raw-body'
 const ProxyRouter = express.Router()
 
@@ -18,6 +20,27 @@ ProxyRouter.route(['/:file', '/*/:file']).get(async (req, res) => {
     return res.send('image not found')
   }
   let mimeType = image ? image.contentType : 'image/png'
+  if (req.headers.range) {
+    // this is extremely hacky but it works.
+    console.log(req.headers.range)
+    let [url] = await bucket.file(req.params.file).getSignedUrl({
+      action: 'read',
+      expires: moment().add(5, 'minutes').toDate(),
+    })
+    let fres = await fetch(url, {
+      headers: {
+        range: req.headers.range,
+      },
+    })
+    let data = await fres.buffer()
+    console.log(data)
+    return res
+      .status(fres.status)
+      .contentType(mimeType)
+      .set('Content-Range', fres.headers.get('Content-Range')!)
+      .set('Content-Length', fres.headers.get('Content-Length')!)
+      .send(data)
+  }
   let [file] = await bucket.file(req.params.file).get()
   let buf = await rb(file.createReadStream())
   return res.contentType(mimeType).send(buf)
