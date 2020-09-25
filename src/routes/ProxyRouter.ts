7eab07b6665f5a438d8f4c39199642293224bc32
relative <relative@pxl.blue>
2020-09-24 20:51:11 -0400

    feat: only search for nondeleted imgs and seek working for all videos

--- src/routes/ProxyRouter.ts
+++ src/routes/ProxyRouter.ts
@@ -3,6 +3,7 @@ import { Image } from '../database/entities/Image'
 import { bucket } from '../util/StorageUtil'
 import fetch from 'node-fetch'
 import moment from 'moment'
+import path from 'path'
 import rb from 'raw-body'
 const ProxyRouter = express.Router()
 
@@ -14,22 +15,22 @@ ProxyRouter.route(['/:file', '/*/:file']).get(async (req, res) => {
   let image = await Image.findOne({
     where: {
       path: req.params.file,
+      deleted: false,
     },
   })
   if (!image) {
     return res.send('image not found')
   }
   let mimeType = image ? image.contentType : 'image/png'
-  if (req.headers.range) {
+  if (req.headers.range || image.contentType.startsWith('video/')) {
     // this is extremely hacky but it works.
-    console.log(req.headers.range)
     let [url] = await bucket.file(req.params.file).getSignedUrl({
       action: 'read',
       expires: moment().add(5, 'minutes').toDate(),
     })
     let fres = await fetch(url, {
       headers: {
-        range: req.headers.range,
+        range: req.headers.range!,
       },
     })
     let data = await fres.buffer()
