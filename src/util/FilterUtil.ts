6336f5231798228cd60ad01d8baa5c25a1dbb4af
seth <relative@pxl.blue>
2020-10-10 17:40:34 -0400

    feat: add more banned words lol

--- src/util/FilterUtil.ts
+++ src/util/FilterUtil.ts
@@ -3,6 +3,15 @@ import Filter from 'bad-words'
 const filter = new Filter()
 
 // self explanatory
-filter.addWords('elerium', 'astral', 'pay', 'pays', 'privateugc', 'wizardugc')
+filter.addWords(
+  'elerium',
+  'astral',
+  'pay',
+  'pays',
+  'privateugc',
+  'wizardugc',
+  'filepush',
+  'fp'
+)
 
 export default filter
