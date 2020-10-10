3fd34e768cfc06b6416d14715fba974aece8d43f
seth <relative@pxl.blue>
2020-10-10 17:34:39 -0400

    fix: update banned words

--- src/util/FilterUtil.ts
+++ src/util/FilterUtil.ts
@@ -3,6 +3,6 @@ import Filter from 'bad-words'
 const filter = new Filter()
 
 // self explanatory
-filter.addWords('elerium', 'astral', 'pay', 'privateugc', 'wizardugc')
+filter.addWords('elerium', 'astral', 'pay', 'pays', 'privateugc', 'wizardugc')
 
 export default filter
