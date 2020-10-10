db995b5799c1f74a0f2ce683bb2f651899c0ee04
seth <relative@pxl.blue>
2020-10-10 14:55:15 -0400

    feat: create Testimonial db model

---
+++ src/database/entities/Testimonial.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  BaseEntity,
+  PrimaryColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+
+@Entity()
+export class Testimonial extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  author: number
+
+  @Column('timestamp with time zone')
+  createdAt: Date
+
+  @Column({ default: true })
+  approved: boolean
+
+  @Column({ length: '100' })
+  testimonial: string
+
+  serialize() {
+    return {
+      id: this.id,
+      author: this.author,
+      createdAt: this.createdAt,
+      approved: this.approved,
+      testimonial: this.testimonial,
+    }
+  }
+}
