1000a0a0bb6900140774d9ec7239a4168bbbaef3
seth <relative@pxl.blue>
2020-10-15 10:58:49 -0400

    feat(proxy): add shorturl support

--- src/routes/ProxyRouter.ts
+++ src/routes/ProxyRouter.ts
@@ -5,6 +5,9 @@ import fetch from 'node-fetch'
 import moment from 'moment'
 import path from 'path'
 import rb from 'raw-body'
+import { ShortURL } from '../database/entities/ShortURL'
+import escapeHtml from 'escape-html'
+
 const ProxyRouter = express.Router()
 
 ProxyRouter.route('/').get((req, res) => {
@@ -12,6 +15,32 @@ ProxyRouter.route('/').get((req, res) => {
 })
 
 ProxyRouter.route(['/:file', '/*/:file']).get(async (req, res) => {
+  console.log(path.basename(req.params.file))
+  if (!path.basename(req.params.file).includes('.')) {
+    // treat as a ShortURL
+    let url = await ShortURL.findOne({
+      where: {
+        shortId: req.params.file.endsWith('+')
+          ? req.params.file.slice(0, -1)
+          : req.params.file,
+      },
+    })
+    if (!url) {
+      return res.send('url not found')
+    }
+    let reveal = req.params.file.endsWith('+')
+    if (reveal) {
+      return res.send(
+        `<p>This link will send you to <code>${escapeHtml(
+          url.destination
+        )}</code>. Are you sure you want to continue?</p><a href="${escapeHtml(
+          url.destination
+        )}">Proceed to ${escapeHtml(url.destination)}</a>`
+      )
+    }
+
+    return res.redirect(url.destination)
+  }
   let image = await Image.findOne({
     where: {
       path: req.params.file,
