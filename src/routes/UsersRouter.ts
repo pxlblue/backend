725742aa62467e30c896c93a900dc42ceb3bc98d
relative <relative@pxl.blue>
2020-12-04 01:56:15 -0500

    fix(users): fix imports

--- src/routes/UsersRouter.ts
+++ src/routes/UsersRouter.ts
@@ -11,7 +11,7 @@ import { bucket } from '../util/StorageUtil'
 import { Testimonial } from '../database/entities/Testimonial'
 import filter from '../util/FilterUtil'
 import { ShortURL } from '../database/entities/ShortURL'
-import { sendMail, verifyEmailTemplate } from 'src/util/MailUtil'
+import { sendMail, verifyEmailTemplate } from '../util/MailUtil'
 
 const valid_username_regex = /^[a-z0-9]+$/i
 const email_regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
